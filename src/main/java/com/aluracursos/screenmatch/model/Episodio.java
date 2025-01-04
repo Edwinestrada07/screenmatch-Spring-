e93ebf3b47c707867f4394c1e6888ed1b68bc763
Edwinestrada07 <r.1993andres.ee@gmail.com>
2025-01-04 11:58:26 -0500

    update principal / Episodios

--- src/main/java/com/aluracursos/screenmatch/model/Episodio.java
+++ src/main/java/com/aluracursos/screenmatch/model/Episodio.java
@@ -1,26 +1,30 @@
 package com.aluracursos.screenmatch.model;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 public class Episodio {
-    private String titulo;
     private Integer temporada;
+    private String titulo;
     private Integer numeroEpisodio;
     private Double evaluacion;
-    private localDate fechaDeLanzamiento;
+    private LocalDate fechaDeLanzamiento;
 
     public Episodio(Integer numero, DatosEpisodio d) {
         this.temporada = numero;
         this.titulo = d.titulo();
         this.numeroEpisodio = d.numeroDeEpisodio();
-        this.evaluacion = Double.valueOf(d.evaluacion());
-        this.fechaDeLanzamiento = LocalDate.parse(d.fechaDeLanzamiento());
-    }
+        try{
+            this.evaluacion = Double.valueOf(d.evaluacion());
+        }catch (NumberFormatException e){
+            this.evaluacion = 0.0;
+        }
+        try{
+            this.fechaDeLanzamiento = LocalDate.parse(d.fechaDeLanzamiento());
+        } catch (DateTimeParseException e){
+            this.fechaDeLanzamiento = null;
+        }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 
     public Integer getTemporada() {
@@ -29,6 +33,14 @@ public class Episodio {
 
     public void setTemporada(Integer temporada) {
         this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     public Integer getNumeroEpisodio() {
@@ -47,21 +59,21 @@ public class Episodio {
         this.evaluacion = evaluacion;
     }
 
-    public localDate getFechaDeLanzamiento() {
+    public LocalDate getFechaDeLanzamiento() {
         return fechaDeLanzamiento;
     }
 
-    public void setFechaDeLanzamiento(localDate fechaDeLanzamiento) {
+    public void setFechaDeLanzamiento(LocalDate fechaDeLanzamiento) {
         this.fechaDeLanzamiento = fechaDeLanzamiento;
     }
 
     @Override
     public String toString() {
         return
-                "titulo='" + titulo + '\'' +
-                ", temporada=" + temporada +
-                ", numeroEpisodio=" + numeroEpisodio +
-                ", evaluacion=" + evaluacion +
-                ", fechaDeLanzamiento=" + fechaDeLanzamiento;
+                "temporada=" + temporada +
+                        ", titulo='" + titulo + '\'' +
+                        ", numeroEpisodio=" + numeroEpisodio +
+                        ", evaluacion=" + evaluacion +
+                        ", fechaDeLanzamiento=" + fechaDeLanzamiento;
     }
 }
